aa32f234a88e148ec75c1b6a35fd8352a73ae57e
nastya <nast9ln@vk.com>
2022-12-24 20:00:12 +0300

    Добавлены тестовые данные

--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #9. GIT\n";
     cout << "Variant #1. Marathon results\n";
     cout << "Author: Anastasia Stuk\n";
+    cout << "Group: 14\n";
     return 0;
 }
